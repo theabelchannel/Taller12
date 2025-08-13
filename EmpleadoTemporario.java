d6628c996f9f7686bf01fbe0512d2c7b6c99a797
miguelmml12 <152296284+miguelmml12@users.noreply.github.com>
2025-08-13 15:52:23 -0500

    Refactoring de EmpleadoTemporario.java

--- EmpleadoTemporario.java
+++ EmpleadoTemporario.java
@@ -1,28 +1,17 @@
 public class EmpleadoTemporario extends Empleado {
-    private String nombre;
-    private String genero;
-    private double salarioBase;
-    private int horasTrabajadas;
-    private String departamento;
     private int mesesContrato;
 
-    public EmpleadoTemporario(String nombre, double salarioBase, int horasTrabajadas, String departamento, int mesesContrato, String genero) {
-        this.nombre = nombre;
-        this.salarioBase = salarioBase;
-        this.horasTrabajadas = horasTrabajadas;
-        this.departamento = departamento;
+    public EmpleadoTemporario(String nombre, double salarioBase, int horasTrabajadas, Departamento departamento, int mesesContrato, String genero) {
+        super(nombre, salarioBase, horasTrabajadas, departamento, genero);
         this.mesesContrato = mesesContrato;
-        this.genero = genero;
     }
 
+    @Override
     public void imprimirDetalles() {
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Genero: " + super.getNombre());
-        System.out.println("Salario: " + salarioBase);
-        System.out.println("Horas trabajadas: " + horasTrabajadas);
-        System.out.println("Departamento: " + departamento);
+        super.imprimirDetalles();
         System.out.println("Meses de contrato: " + mesesContrato);
     }
 
     // Más metodos
 }
+
