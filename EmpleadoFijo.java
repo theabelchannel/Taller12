eb177efecd8d567f78271a33bcb0208232eeda59
miguelmml12 <152296284+miguelmml12@users.noreply.github.com>
2025-08-13 15:53:55 -0500

    Refactoring de EmpleadoFijo.java

--- EmpleadoFijo.java
+++ EmpleadoFijo.java
@@ -1,21 +1,16 @@
 public class EmpleadoFijo extends Empleado {
     private double bonoAnual;
-    private String genero;
 
-    public EmpleadoFijo(String nombre, double salarioBase, int horasTrabajadas, String departamento, double bonoAnual, String genero) {
-        super(nombre, salarioBase, horasTrabajadas, 0, departamento);
+    public EmpleadoFijo(String nombre, double salarioBase, int horasTrabajadas, Departamento departamento, String genero, double bonoAnual) {
+        super(nombre, salarioBase, horasTrabajadas, departamento,genero);
         this.bonoAnual = bonoAnual;
-        this.genero = genero;
+        
     }
 
+    @Override
     public void imprimirDetalles() {
-        System.out.println("Nombre: " + super.getNombre());
-        System.out.println("Genero: " + super.getNombre());
-        System.out.println("Salario: " + super.getSalarioBase());
-        System.out.println("Horas trabajadas: " + super.getHorasTrabajadas());
-        System.out.println("Departamento: " + super.getDepartamento());
-        System.out.println("bonoAnual: " + bonoAnual);
+        super.imprimirDetalles();
+        System.out.println("Bono Anual: " + bonoAnual);
     }
 
-    // Más metodos
 }
